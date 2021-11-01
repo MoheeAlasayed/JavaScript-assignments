a00e6deb205bdb319d249c98525307fbd77d5075
MoheeAlasayed <91501960+MoheeAlasayed@users.noreply.github.com>
2021-11-01 16:18:38 -0700

    assignments 2

---
+++ CH-2/js/main.js
@@ -0,0 +1,31 @@
+let NumberOne = 10 ,
+    NumberTow = 20;
+console.log(typeof(NumberOne));
+console.log(typeof(NumberTow));
+console.log(NumberOne.toString()+NumberTow.toString());//Convert Number To String
+console.log(typeof((NumberOne.toString()))+typeof(NumberTow.toString())); //Normal Concatenate 
+console.log(`${NumberOne.toString()}${NumberTow.toString()}`); //Template Literals
+console.log(`${typeof((NumberOne.toString()))}${typeof(NumberTow.toString())}`);//Template Literals
+console.log(('\n'+ NumberTow +'\n' +NumberOne).repeat(20)); //Normal Concatenate 
+console.log((`
+${NumberTow}
+${NumberOne}`).repeat(20));//Template Literals
+
+console.log(elzero.innerHTML); // We Add ID in html File (elzero)
+console.log(typeof elzero); 
+
+
+
+console.log('`I\'m In \n\\\\\nLove \\\\ \"\"\" \'\'\'\n++ WITH ++\n\\\"\"\"\\\"\"\"\n\"\"JavaScript\"\"\"\`\`\`');
+
+
+let a = 21;
+let b = 20;
+console.log('_'+a+'_'+b.toString()+a.toString()+'_'+b.toString()+a.toString()+'_'+b+'_');
+
+
+
+
+
+
+
